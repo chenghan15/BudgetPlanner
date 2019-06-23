c7178d5fee26064798cd156e051789d9e8e29a20
chenghan15 <hanwfifteen@gmail.com>
2019-06-23 23:42:37 +1200

    small change

--- app/src/main/java/com/cheng/budgetplanner/activity/AboutActivity.java
+++ app/src/main/java/com/cheng/budgetplanner/activity/AboutActivity.java
@@ -31,8 +31,8 @@ public class AboutActivity extends BaseActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
     }
